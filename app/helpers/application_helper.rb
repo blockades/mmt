5ca97e3aab89d3eff78c62ff7648db26f2ca99ff
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-08-06 11:23:01 +0100

    Setup Devise for user authentication, default root path to users#index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def flash_notices
+    raw([:notice, :error, :alert].map { |type| content_tag('div', flash[type], id: type) unless flash[type].blank?  }.join)
+  end
+
 end
