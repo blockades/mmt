e33ee931a434c20bfd2ba0b4e8940ee9fde7a551
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-08-10 12:21:10 +0100

    Add cryto amount to a given holding

--- db/migrate/20170806185624_create_holdings.rb
+++ db/migrate/20170806185624_create_holdings.rb
@@ -2,14 +2,16 @@ class CreateHoldings < ActiveRecord::Migration[5.0]
   def change
     create_table :holdings do |t|
       t.integer :user_plan_id
-      # The amount they bought
+      t.integer :coin_id
+      t.integer :admin_id
+
       t.decimal :amount, precision: 10, scale: 2, default: 0.0
-      # The currency in which the purchase was made
+      t.decimal :crypto_amount, precision: 20, scale: 15, default: 0.0
+
       t.string :currency, limit: 50, default: 'United States Dollar'
       t.string :iso_currency, limit: 4, default: 'USD'
       t.string :sale_type, default: 'B'
 
-      t.integer :admin_id
       t.boolean :iou, default: false
 
       t.timestamps
