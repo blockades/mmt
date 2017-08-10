390e6c2f6573f9980f75c1602b10ce9baffad866
Kieran Gibb <kieran.a.gibb@gmail.com>
2017-08-10 11:46:00 +0100

    Configure server to run on port 9999

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,12 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'rails/commands/server'
+
+module DefaultOptions
+  def default_options
+    super.merge!(Host: '0.0.0.0', Port: 9999)
+  end
+end
+
+Rails::Server.send(:prepend, DefaultOptions)
