ae09e1d95a50396b2f33d4b66d151315292a8aa1
Nikolai B <nikolai-b@users.noreply.github.com>
2017-08-26 21:39:32 +0100

    One holding per coin (per portfolio)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817184722) do
+ActiveRecord::Schema.define(version: 20170826132553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20170817184722) do
     t.boolean  "withdrawal",                                default: false, null: false
     t.integer  "portfolio_id",                                              null: false
     t.integer  "quantity",                                                  null: false
-    t.index ["coin_id"], name: "index_holdings_on_coin_id", using: :btree
+    t.index ["coin_id", "portfolio_id"], name: "index_holdings_on_coin_id_and_portfolio_id", unique: true, using: :btree
     t.index ["portfolio_id"], name: "index_holdings_on_portfolio_id", using: :btree
   end
 
