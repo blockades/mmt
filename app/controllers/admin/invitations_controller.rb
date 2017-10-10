135b0f5ea42706cb20ae68cb6de516d3a79d78d6
Kieran <kieran.a.gibb@gmail.com>
2017-10-10 17:46:25 +0100

    Add actions on controller for invitations

--- app/controllers/admin/invitations_controller.rb
+++ app/controllers/admin/invitations_controller.rb
@@ -1,5 +1,21 @@
 module Admin
   class InvitationsController < Devise::InvitationsController
     before_action :verify_admin
+
+    def index
+      @not_accepted = Member.invitation_not_accepted
+      @recently_accepted = Member.invitation_accepted
+    end
+
+    def create
+      member = Member.invite! invitation_params
+      redirect_to admin_members_path, notice: "Invitation sent to #{member.email}"
+    end
+
+    private
+
+    def invitation_params
+      params.require(:member).permit(:email)
+    end
   end
 end
