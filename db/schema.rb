75b265933046a2328fa43c27393be45ebc15f5a3
Kieran <kieran.a.gibb@gmail.com>
2017-10-10 16:11:54 +0100

    Update schema for devise_invitable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826132553) do
+ActiveRecord::Schema.define(version: 20171010132035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,18 @@ ActiveRecord::Schema.define(version: 20170826132553) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "admin"
+    t.string   "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer  "invitation_limit"
+    t.string   "invited_by_type"
+    t.integer  "invited_by_id"
+    t.integer  "invitations_count",      default: 0
     t.index ["email"], name: "index_members_on_email", unique: true, using: :btree
+    t.index ["invitation_token"], name: "index_members_on_invitation_token", unique: true, using: :btree
+    t.index ["invitations_count"], name: "index_members_on_invitations_count", using: :btree
+    t.index ["invited_by_id"], name: "index_members_on_invited_by_id", using: :btree
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true, using: :btree
   end
 
