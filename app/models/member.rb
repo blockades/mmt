e3cce310d26473602ae9c23bdf301f85473aac31
Kieran <kieran.a.gibb@gmail.com>
2017-10-11 13:36:20 +0100

    capitalize SQL

--- app/models/member.rb
+++ app/models/member.rb
@@ -19,7 +19,7 @@ class Member < ApplicationRecord
     def find_for_database_authentication(warden_conditions)
       conditions = warden_conditions.dup
       if login = conditions.delete(:login)
-        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase  }]).first
+        where(conditions.to_h).where(["LOWER(username) = :value OR LOWER(email) = :value", { value: login.downcase }]).first
       elsif conditions.has_key?(:username) || conditions.has_key?(:email)
         where(conditions.to_h).first
       end
