44d357fca7946d23195e017308e1ea97bd042e48
Kieran <kieran.a.gibb@gmail.com>
2017-10-11 11:59:27 +0100

    Reorganise routes to scope members & rename admin to admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :members, skip: [:registrations], controllers: { invitations: 'admin/invitations' }
+  devise_for :members, skip: [:registrations], controllers: { invitations: 'admins/invitations' }
 
-  root to: "members#index"
-  resources :holdings
+  scope module: :members do
+    root to: "dashboard#index"
+    resources :members, only: [:edit, :update]
+    resources :holdings, only: [:index, :edit]
+    resources :portfolios, only: [:index, :edit]
+  end
 
-  namespace :admin do
-    resources :dashboard, only: [:index]
+  namespace :admins do
+    root to: 'dashboard#index'
     resources :portfolios, only: [:index, :new, :create, :show]
     resources :coins, only: [:index, :edit, :update]
     resources :members, only: [:index, :new, :create, :edit, :update]
