699f54e3516f420d542693ce3371b75ffa9d55c7
Kieran <kieran.a.gibb@gmail.com>
2017-10-13 13:37:09 +0100

    Add update action on members controller

--- app/controllers/members/members_controller.rb
+++ app/controllers/members/members_controller.rb
@@ -2,7 +2,7 @@
 
 module Members
   class MembersController < ApplicationController
-    before_action :find_member, only: [:show, :edit]
+    before_action :find_member, only: [:show, :edit, :update]
 
     def index
       @members = Member.all
@@ -11,10 +11,26 @@ module Members
     def show
     end
 
+    def update
+      respond_to do |format|
+        if @member.update member_params
+          format.html { redirect_to member_path(@member), notice: "Successfull updated" }
+          format.json { render json: { success: true, member: @member }.to_json }
+        else
+          format.html { redirect_to member_path(@member), error: "Failed to update" }
+          format.json { render json: { success: false, errors: @member.errors, member: @member }.to_json }
+        end
+      end
+    end
+
     private
 
     def find_member
       @member = params[:id] ? Member.friendly.find(params[:id]) : current_member
+    end
+
+    def member_params
+      params.require(:member).permit(:username)
     end
   end
 end
