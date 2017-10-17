9db1e58dfef2dfd3477ebd71c093bdfdd0597bb0
ameba23 <ameba23@systemli.org>
2017-10-17 19:18:07 +0200

    Added usernames to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Member.destroy_all
 
-Member.create email: 'admin@mmt.blockades.org', password: 'admin123', admin: true
-Member.create email: 'bob.j.bobess@farcical.net', password: '123456'
-Member.create email: 'j.a.funky@hotmail.net', password: '123456'
-Member.create email: 'danny@jman.net', password: '123456'
+Member.create username: 'admin1', email: 'admin@mmt.blockades.org', password: 'admin123', admin: true
+Member.create username: 'bob', email: 'bob.j.bobess@farcical.net', password: '123456'
+Member.create username: 'funky', email: 'j.a.funky@hotmail.net', password: '123456'
+Member.create username: 'danny', email: 'danny@jman.net', password: '123456'
 
