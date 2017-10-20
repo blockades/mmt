9dda36a6a009237415ba7ea1d49ba5f93697368e
Kieran <kieran.a.gibb@gmail.com>
2017-10-20 16:14:40 +0100

    Controller to execute command

--- app/controllers/members/portfolios_controller.rb
+++ app/controllers/members/portfolios_controller.rb
@@ -2,8 +2,28 @@
 
 module Members
   class PortfoliosController < ApplicationController
-    def show
-      @portfolio = current_member.live_portfolio
+
+    def index
+      @portfolios = Portfolio.all
     end
+
+    def new
+      @portfolio_id = SecureRandom.uuid
+      @coins = Coin.all
+      @members = Member.all
+    end
+
+    def add_asset
+      execute Command::AddAssetToPorfolio.new(asset_params)
+      head :ok
+    end
+
+    private
+
+    def asset_params
+      parameters = params.permit(:coin_id, :id)
+      { coin_id: parameters[:coin_id], portfolio_id: parameters[:id] }
+    end
+
   end
 end
