a0cf37680560ebb77541f76abf032865aeef150d
Kieran <kieran.a.gibb@gmail.com>
2017-10-20 16:08:30 +0100

    Add event store table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013101043) do
+ActiveRecord::Schema.define(version: 20170806090614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,19 @@ ActiveRecord::Schema.define(version: 20171013101043) do
     t.integer "subdivision", default: 8, null: false
     t.string "slug"
     t.index ["code"], name: "index_coins_on_code", unique: true
+  end
+
+  create_table "event_store_events", id: :serial, force: :cascade do |t|
+    t.string "stream", null: false
+    t.string "event_type", null: false
+    t.string "event_id", null: false
+    t.text "metadata"
+    t.text "data", null: false
+    t.datetime "created_at", null: false
+    t.index ["created_at"], name: "index_event_store_events_on_created_at"
+    t.index ["event_id"], name: "index_event_store_events_on_event_id", unique: true
+    t.index ["event_type"], name: "index_event_store_events_on_event_type"
+    t.index ["stream"], name: "index_event_store_events_on_stream"
   end
 
   create_table "friendly_id_slugs", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
