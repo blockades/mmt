06892d0ec076ce4249e555146fce15a3e3aaa3be
Kieran <kieran.a.gibb@gmail.com>
2017-10-20 16:47:56 +0100

    Define update_asset_value command

---
+++ app/command/update_asset_value.rb
@@ -0,0 +1,9 @@
+module Command
+  class UpdateAssetValue < Command::Base
+    attr_accessor :coin_id, :portfolio_id
+
+    validates :coin_id, :portfolio_id, presence: true
+
+    alias :aggregate_id :portfolio_id
+  end
+end
