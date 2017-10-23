e773207ee8483412db9a4b846b3d6b93352a723e
Kieran <kieran.a.gibb@gmail.com>
2017-10-23 12:17:25 +0100

    Add hooks on events

--- app/domain/portfolio.rb
+++ app/domain/portfolio.rb
@@ -12,34 +12,32 @@ module Domain
       @assets = []
     end
 
-    def finalise(portfolio_number, member_id)
+    def finalise(member_id)
       raise AlreadySubmitted if state == :finalised
       raise PorfolioExpired if state == :expired
       raise MissingMember unless member_id
-      apply Events::PorfolioFinalised.new(data: { porfolio_id: id, portfolio_number: portfolio_number, member_id: member_id })
+      apply Events::PortfolioFinalised.new(data: { porfolio_id: id, member_id: member_id })
     end
 
     def add_asset(coin_id)
       raise AlreadySubmitted if state == :finalised
-      apply Events::AddAssetToPortfolio.new(data: { portfolio_id: id, coin_id: coin_id })
+      apply Events::AssetAddedToPortfolio.new(data: { portfolio_id: id, coin_id: coin_id })
     end
 
     def remove_asset(coin_id)
       raise AlreadySubmitted if state == :finalised
-      apply Events::RemoveAssetFromPortfolio.new(data: { portfolio_id: id, coin_id: coin_id })
+      apply Events::AssetRemovedFromPortfolio.new(data: { portfolio_id: id, coin_id: coin_id })
     end
 
     attr_reader :id
     private
-    attr_accessor :state, :member_id, :number, :assets
+    attr_accessor :state, :member_id, :assets
 
     def apply_portfolio_finalised(event)
-      @member_id = event.data[:member_id]
-      @number = event.data[:portfolio_number]
       @state = :finalised
     end
 
-    def apply_add_asset_to_portfolio(event)
+    def apply_asset_added_to_portfolio(event)
       coin_id = event.data[:coin_id]
       asset = find_asset(coin_id)
       if asset.any?
@@ -49,7 +47,7 @@ module Domain
       end
     end
 
-    def apply_remove_asset_from_portfolio(event)
+    def apply_asset_removed_from_portfolio(event)
       coin_id = event.data[:coin_id]
       asset = find_asset(coin_id)
       return unless asset.any?
