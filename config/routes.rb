2f927b0876d8846a337f1677a0c73ac2e594b9c6
Kieran <kieran.a.gibb@gmail.com>
2017-10-25 14:48:53 +0100

    Complete signin with authy

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ Rails.application.routes.draw do
 
   devise_for :members, only: :invitations, controllers: { invitations: 'admins/invitations' }
 
-  devise_for :members, skip: [:registrations, :invitations], path: :auth, path_names: {
+  devise_for :members, only: [:sessions, :passwords], path: :auth, path_names: {
     sign_in: :login,
-    sign_out: :logout,
-    verify_authy: :verify_token,
-    enable_authy: :enable_two_factor,
-    verify_authy_installation: :verify_installation
+    sign_out: :logout
+  }
+
+  devise_for :members, skip: [:registrations, :invitations, :sessions, :passwords], path_names: {
+    verify_token: '/verify_token',
+    verify_authy: '/verify_authy',
+    enable_authy: '/enable_two_factor',
+    verify_authy_installation: '/verify_installation'
   }
 
   scope module: :members do
