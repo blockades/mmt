000b54a62b8fa8b1099dd0ca8b9981aadebeed10
Kieran <kieran.a.gibb@gmail.com>
2017-10-26 12:09:32 +0100

    Add workflow boolean and phone number (for twilio auth)

--- db/migrate/20171026090830_add_devise_two_factor_to_members.rb
+++ db/migrate/20171026090830_add_devise_two_factor_to_members.rb
@@ -5,5 +5,9 @@ class AddDeviseTwoFactorToMembers < ActiveRecord::Migration[5.1]
     add_column :members, :encrypted_otp_secret_salt, :string
     add_column :members, :consumed_timestep, :integer
     add_column :members, :otp_required_for_login, :boolean
+    add_column :members, :otp_setup_initiated, :boolean
+
+    add_column :members, :country_code, :string
+    add_column :members, :phone_number, :string
   end
 end
