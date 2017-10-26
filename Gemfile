ae96b36616dbd9ae8cc810c11e1234d8179bd13c
Kieran <kieran.a.gibb@gmail.com>
2017-10-26 16:42:37 +0100

    Bundle in rqrcode gem and locally produced qr_codes

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem "sidekiq"
 gem "devise"
 gem "devise_invitable"
 gem 'two_factor_authentication'
+gem 'rqrcode-with-patches', '~> 0.5.4'
 
 gem "friendly_id", "~> 5.1.0"
 gem "interactor-rails"
--- app/helpers/two_factor_helper.rb
+++ app/helpers/two_factor_helper.rb
@@ -2,14 +2,24 @@
 
 module TwoFactorHelper
   def provisioning_uri
-    current_member.provisioning_uri("MagicMoneyTree:#{current_member.email}")
+    Rack::Utils.escape current_member.provisioning_uri("#{Rails.application.class.parent_name}:#{current_member.email}")
   end
 
   def google_authenticator_qrcode
     image_tag(
-      "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{Rack::Utils.escape(provisioning_uri)}",
+      "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{provisioning_uri}",
       alt: 'Google Authenticator QRCode'
     )
+  end
+
+  def local_qrcode
+    # %%TODO%% This doesn't work. Invalid code.
+    RQRCode::QRCode.new(provisioning_uri).as_svg(
+      offset: 0,
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 4
+    ).html_safe
   end
 end
 
