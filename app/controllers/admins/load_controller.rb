a24a87d7412bc254945e7c9c0152ddda8e49af2c
Kieran <kieran.a.gibb@gmail.com>
2017-11-10 17:15:04 +0000

    Controllers for load / exchange / withdrawl / allocation

--- app/controllers/admins/load_controller.rb
+++ app/controllers/admins/load_controller.rb
@@ -1,25 +1,28 @@
 # frozen_string_literal: true
 
 module Admins
-  class LoadController < AdminController
+  class LoadController < AdminsController
     before_action :find_coin
 
     def new
     end
 
     def create
+      unless permitted_params[:destination_quantity].present?
+        redirect_back fallback_location: admins_new_coin_load_path(@coin), alert: 'Quantity required' and return
+      end
       command = Command::Transaction::Load.new(load_params)
       execute command
-      redirect_to admins_coin_path(@coin), notice: 'Success'
+      redirect_to admins_coins_path(@coin), notice: 'Success'
     rescue Command::ValidationError => error
       Rails.logger.error(error)
-      redirect_to admins_new_load_coin_path(@coin), alert: 'Fail'
+      redirect_to admins_new_coin_load_path(@coin), alert: 'Fail'
     end
 
     private
 
     def find_coin
-      @coin = Coin.find params[:coin_id]
+      @coin = Coin.friendly.find(params[:coin_id]).decorate
     end
 
     def permitted_params
@@ -28,10 +31,14 @@ module Admins
 
     def load_params
       permitted_params.merge!(
-        destination_quantity: permitted_params.try(:destination_quantity).try(:*, 10**@coin.subdivision),
-        destination_coin_id: @coin.id
-      )
+        destination_quantity: quantity_as_integer,
+        destination_coin_id: @coin.id,
+        admin_id: current_member.id,
+      ).to_h.symbolize_keys
     end
 
+    def quantity_as_integer
+      (permitted_params.fetch(:destination_quantity).to_d * 10**@coin.subdivision).to_i
+    end
   end
 end
