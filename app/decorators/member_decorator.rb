aada8c94021dfcae689b20862e9422757c4a8b5b
Kieran <kieran.a.gibb@gmail.com>
2017-11-10 17:15:45 +0000

    Helper methods for view - show quantites as decimals

--- app/decorators/member_decorator.rb
+++ app/decorators/member_decorator.rb
@@ -3,6 +3,10 @@
 class MemberDecorator < Draper::Decorator
   delegate_all
 
+  def coin_balance(coin)
+    holdings(coin.id).to_d / 10**coin.subdivision
+  end
+
   def two_factor_delivery_method
     Member::TWO_FACTOR_DELIVERY_METHODS[otp_delivery_method].downcase
   end
