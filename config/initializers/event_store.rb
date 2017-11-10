81870d7ca66f433a3353c7e2d4745045d7a01083
Kieran <kieran.a.gibb@gmail.com>
2017-11-10 17:16:49 +0000

    subscribe to transactions for relevant changes to coin/member streams

--- config/initializers/event_store.rb
+++ config/initializers/event_store.rb
@@ -3,7 +3,10 @@
 Rails.application.configure do
 
   config.event_store.tap do |event_store|
-    # event_store.subscribe(Subscribers::ETC)
+    event_store.subscribe(Subscribers::Transaction::Load, [Events::Transaction::Load])
+    event_store.subscribe(Subscribers::Transaction::Allocate, [Events::Transaction::Allocate])
+    event_store.subscribe(Subscribers::Transaction::Exchange, [Events::Transaction::Exchange])
+    event_store.subscribe(Subscribers::Transaction::Withdraw, [Events::Transaction::Withdraw])
   end
 
 end
