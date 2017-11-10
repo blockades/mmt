d8f1fe5b31bdff29eedbb084ab1e496dc3d52466
Kieran <kieran.a.gibb@gmail.com>
2017-11-10 17:15:45 +0000

    Helper methods for view - show quantites as decimals

---
+++ app/decorators/coin_decorator.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class CoinDecorator < Draper::Decorator
+  delegate_all
+
+  def member_balance
+    h.current_member.holdings(id).to_d / 10**subdivision
+  end
+
+  def central_reserve
+    reserves.to_d / 10**subdivision
+  end
+
+  def reserves_available
+    holdings.to_d / 10**subdivision
+  end
+end
