9f3780b93a8e1010fcddd1d6c8313a59975886ae
Kieran <kieran.a.gibb@gmail.com>
2017-11-10 17:15:04 +0000

    Controllers for load / exchange / withdrawl / allocation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_member!
   before_action :store_return_paths
 
+  include Command::Execute
+
   protected
 
   def configure_permitted_parameters
