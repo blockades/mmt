62f71ba022f7a8d443ef401fe9fa52e6a184a1cc
Kieran <kieran.a.gibb@gmail.com>
2017-11-29 18:23:33 +0000

    Migration for polymorphic associations on transaction table

--- db/migrate/20171116122152_create_coin_events.rb
+++ db/migrate/20171116122152_create_coin_events.rb
@@ -3,8 +3,7 @@ class CreateCoinEvents < ActiveRecord::Migration[5.1]
     create_table :coin_events, id: :uuid do |t|
       t.references :coin, type: :uuid, foreign_key: true, index: true
       t.references :transaction, type: :uuid, foreign_key: true, index: true
-      t.integer :liability, limit: 8
-      t.integer :available, limit: 8
+      t.integer :assets, limit: 8
 
       t.timestamps
     end
--- db/migrate/20171116122309_create_member_coin_events.rb
+++ db/migrate/20171116122309_create_member_coin_events.rb
@@ -4,6 +4,8 @@ class CreateMemberCoinEvents < ActiveRecord::Migration[5.1]
       t.references :coin, type: :uuid, foreign_key: true, index: true
       t.references :member, type: :uuid, foreign_key: true, index: true
       t.references :transaction, type: :uuid, foreign_key: true, index: true
+      # %%TODO%% Move admin_liability into a fresh table admin_coin_events
+      # t.integer :admin_liability, limit: 8
       t.integer :liability, limit: 8
       t.decimal :rate
 
