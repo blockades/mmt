6d5bb3dbed57b9af1ef3dbbcfeffd0f12384ce54
Nikolai B <nikolai-b@users.noreply.github.com>
2017-12-07 20:15:24 +0000

    Do not migrate the db in the entry point
    This should be a seaprate step

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,5 +7,4 @@ if [ -f /app/tmp/pids/server.pid ]; then
   rm /app/tmp/pids/server.pid
 fi
 
-bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
-docker/wait-for-postgresql.sh postgres bundle exec rails server
+docker/wait-for-postgresql.sh bundle exec rails server
