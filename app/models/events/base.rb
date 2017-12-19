85c1f3dff9e06bbf85275981534cb8d111c905a1
Kieran <kieran.a.gibb@gmail.com>
2017-12-19 16:03:28 +0000

    Remove unused methods and add event total sum class method

--- app/models/events/base.rb
+++ app/models/events/base.rb
@@ -8,11 +8,11 @@ module Events
     self.table_name = "events"
 
     belongs_to :coin
-    belongs_to :system_transaction
     belongs_to :member
+    belongs_to :system_transaction
 
-    scope :credit, -> { where("entry > 0") }
-    scope :debit, -> { where("entry < 0") }
+    scope :credit, -> { where(arel_table[:entry].gt(0)) }
+    scope :debit, -> { where(arel_table[:entry].lt(0)) }
     scope :with_coin, ->(coin_id) { where coin_id: coin_id }
     scope :with_coins, -> { joins(:coin) }
     scope :crypto, -> { with_coins.merge(Coin.crypto) }
@@ -20,37 +20,13 @@ module Events
 
     TYPES = %w[Equity Asset Liability].freeze
 
-    TYPES.each do |type|
-      scope type.underscore.to_sym, -> { where type: type }
-
-      define_method "#{type.underscore}?" do
-        type == self.type
-      end
-    end
-
     validates :type, presence: true,
                      inclusion: { in: TYPES }
 
     validates_associated :coin, :member
 
-    def credit?
-      send(type).positive?
-    end
-
-    def debit?
-      send(type).negative?
-    end
-
-    def system_liability
-      Events::Liability.sum(:liability)
-    end
-
-    def system_equity
-      Events::Equity.sum(:equity)
-    end
-
-    def system_assets
-      Events::Asset.sum(:assets)
+    def self.total
+      sum(:entry)
     end
   end
 end
