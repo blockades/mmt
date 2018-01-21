f00e6da410b97cb004f34edbdfa983810841716a
Nikolai B <nikolai-b@users.noreply.github.com>
2018-01-21 21:47:04 +0000

    Limit exchange amount to buy in JS

--- app/assets/javascripts/exchanges.js
+++ app/assets/javascripts/exchanges.js
@@ -20,8 +20,9 @@ var Exchange = function (coin) {
     destinationQuantity = (cost * sourceRate) / destinationRate;
     $('#destinationQuantityDisplay').val(destinationQuantity);
     $('#destinationQuantity').val(cost * Math.pow(10, destinationSubdivision));
-    $('.balance').text(sourceCoin.assets / Math.pow(10, sourceSubdivision) + " " + sourceCoin.code);
-
+    var assets = sourceCoin.assets / Math.pow(10, sourceSubdivision);
+    $('.balance').text(assets + " " + sourceCoin.code);
+    $("#destinationQuantityDisplay").attr({max: assets * sourceRate / destinationRate});
     $('#sourceQuantityDisplay').val(cost);
     $('#sourceQuantity').val(cost * Math.pow(10, sourceSubdivision));
     $('#destinationQuantity').val(destinationQuantity * Math.pow(10, destinationSubdivision));
