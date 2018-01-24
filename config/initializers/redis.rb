011751b23525e6ab524d9e07585a5821b478a28f
Kieran <kieran.a.gibb@gmail.com>
2018-01-24 13:47:33 +0000

    Reconfigure to use uri in redis host

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-redis_host = ENV.fetch("REDIS_HOST") { "localhost" }
+redis_uri = URI.parse(ENV.fetch("REDIS_URL") { "http://localhost:3000/12" })
 
 Rails.application.configure do
-  config.cache_store = :redis_store, { host: redis_host, port: 6379, db: 0 }
-  config.session_store :redis_store, expires_in: 1.day, servers: [{ db: 0, host: redis_host }]
+  config.cache_store = :redis_store, { host: redis_uri.host, port: redis_uri.port, db: 0 }
+  config.session_store :redis_store, expires_in: 1.day, servers: [{ db: 0, host: redis_uri.host }]
 end
